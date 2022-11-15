0ecaa078b4cc10eeaaea3dfe05d9152898ce0a9b
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-11-15 18:36:43 +0500

    all code but not working

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers";
+import { application } from "./application";import * as bootstrap from "bootstrap"
+
